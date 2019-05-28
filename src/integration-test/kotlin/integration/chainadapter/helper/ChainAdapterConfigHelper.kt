6f9db8b2efd3146193cc08b0b932082829425233
Alexey <Alexey.N.Chernyshov@gmail.com>
2019-05-28 15:23:08 +0300

    add license

--- src/integration-test/kotlin/integration/chainadapter/helper/ChainAdapterConfigHelper.kt
+++ src/integration-test/kotlin/integration/chainadapter/helper/ChainAdapterConfigHelper.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package integration.chainadapter.helper
 
 import com.d3.chainadapter.config.ChainAdapterConfig
